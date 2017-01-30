6d1ae6cca13d3ebe2af6405f6b1e872c83fb2383
Guillaume Biton <guillaume.biton@free.fr>
2017-01-30 17:00:46 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,113 @@
+<template lang='pug'>
+#app
+  // Status bar overlay for fullscreen mode
+  f7-statusbar
+
+  // Right panel with cover effect
+  f7-panel(right, reveal)
+    f7-block
+      p Right panel content goes here
+      
+  // Left panel with cover effect
+  f7-panel(left, reveal)
+    f7-block
+      p Left panel content goes here
+      
+  // Views
+  f7-views
+    // Left View
+    f7-view(main, navbar-through, :dynamic-navbar='true')
+      f7-navbar
+        f7-nav-center(sliding) Contacts
+      f7-pages
+        f7-page(name='index')
+          f7-list(contacts)
+            f7-list-group(v-for='(group, key) in contacts')
+              f7-list-item(:title='key', group-title)
+              f7-list-item(v-for='name in group', :title='name', @click='selectContact(name)', link='/contact/', :link-view='linkView')
+          
+    // Main View
+    f7-view(navbar-through, :animatePages='!splitView').view-detail
+      // Pages
+      f7-pages
+        // Page, data-page contains page name
+        f7-page(name='main')
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      contacts: {
+        'A': [
+          'Aaron',
+          'Abbie',
+          'Adam',
+          'Adele',
+          'Agatha',
+          'Agnes',
+          'Albert',
+          'Alexander'
+        ],
+        'B': [
+          'Bailey',
+          'Barclay',
+          'Bartolo',
+          'Bellamy',
+          'Belle',
+          'Benjamin'
+        ],
+        'C': [
+          'Caiden',
+          'Calvin',
+          'Candy',
+          'Carl',
+          'Cherilyn',
+          'Chester',
+          'Chloe'
+        ],
+        'V': [
+          'Vladimir'
+        ]
+      },
+      contact: null,
+      splitView: false,
+      linkView: ''
+    }
+  },
+  methods: {
+    checkSplitView () {
+      if (this.$$(window).width() < 767) {
+        this.splitView = false
+        this.linkView = ''
+      } else {
+        this.splitView = true
+        this.linkView = '.view-detail'
+      }
+    }
+  },
+  created () {
+    this.checkSplitView()
+    this.$$(window).resize(this.checkSplitView)
+  }
+}
+</script>
+
+<style src='framework7/dist/css/framework7.ios.css'></style>
+<style src='framework7/dist/css/framework7.ios.colors.css'></style>
+<style lang="sass?indentedSyntax">
+  $panel-width: 320px
+
+  @media('min-width: 769px')
+    .view-main
+      float: left
+      width: $panel-width
+      border-right: 1px solid #b2b2b2
+
+    .view-detail
+      width: calc(100% - #{$panel-width})
+    
+    .navbar .navbar-inner .left
+      display: none
+</style>
---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import 'framework7'
+import Framework7Vue from 'framework7-vue'
+
+import App from './App'
+import ContactPage from './pages/contact'
+
+Vue.use(Framework7Vue)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  framework7: {
+    root: '#app',
+    animateNavBackIcon: true,
+    routes: [
+      {
+        path: '/contact/',
+        component: ContactPage
+      }
+    ]
+  },
+  components: {
+    App
+  },
+  template: '<App/>'
+})
